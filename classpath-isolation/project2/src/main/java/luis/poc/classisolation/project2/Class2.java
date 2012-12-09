57384019777e02cb8494116a733b64e3db52e3cb
mago <mago@anduril.(none)>
2012-12-09 12:54:57 +0100

    Added EJB support for project 2

--- classpath-isolation/project2/src/main/java/luis/poc/classisolation/project2/Class2.java
+++ classpath-isolation/project2/src/main/java/luis/poc/classisolation/project2/Class2.java
@@ -3,6 +3,9 @@ package luis.poc.classisolation.project2;
 import luis.poc.classisolation.project1.Class1;
 import luis.poc.classpathisolation.library.DummyLibrary;
 
+import javax.ejb.Singleton;
+import javax.ejb.Stateless;
+
 /**
  * Created with IntelliJ IDEA.
  * User: mago
@@ -10,12 +13,17 @@ import luis.poc.classpathisolation.library.DummyLibrary;
  * Time: 2:39 PM
  * To change this template use File | Settings | File Templates.
  */
+@Stateless
 public class Class2 {
+
+    static {
+        System.out.println("Static init of Class 2");
+    }
 
     public Class2()  {
         final DummyLibrary dummyLibrary = new DummyLibrary();
         dummyLibrary.doNothing2();
         final Class1 class1 = new Class1();
-
+        System.out.println("Created instance of Class 2");
     }
 }
